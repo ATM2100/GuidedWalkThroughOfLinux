bfaae9e81dd8959fc24fbef0d55d9a98c0a6880e
ATM2100 <alan.macklin@yahoo.com>
2024-12-04 17:48:13 -0500

    script v3

--- scripts/rootScript.sh
+++ scripts/rootScript.sh
@@ -8,10 +8,3 @@ apt install sudo
 mv /home/user/GuidedWalkThroughOfLinux/exercises /home/user
 cd /home/user
 rm -rf /home/user/GuidedWalkThroughOfLinux
-history -c
-
-su - user
-history -c 
-
-su - root
-/sbin/shutdown -h 0
